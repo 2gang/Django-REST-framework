0ac9e4e8c69661b5f7e89e18ebb5e6a7802c2a43
이경호 <rud0503@gmail.com>
2022-08-23 09:21:53 +0900

    0823

--- VueDjAgency/mysite/settings.py
+++ VueDjAgency/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['175.192.165.46', '127.0.0.1', 'localhost']
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'api.apps.ApiConfig',
+    'api2.apps.Api2Config',
     
     'rest_framework',
 
@@ -143,9 +144,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 #DRF
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # # Use Django's standard `django.contrib.auth` permissions,
+    # # or allow read-only access for unauthenticated users.
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
 }
