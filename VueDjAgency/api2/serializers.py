332c6e8988245f93830d24c39319acf6718cf963
이경호 <rud0503@gmail.com>
2022-08-28 15:54:24 +0900

    0828

--- VueDjAgency/api2/serializers.py
+++ VueDjAgency/api2/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post, Comment
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -8,7 +8,18 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
         
-class PostSerializer(serializers.ModelSerializer):
+class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post 
         fields = ['id', 'title', 'image', 'like', 'category']
+        
+class PostRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post 
+        exclude = ['create_dt']
+        
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment 
+        fields = '__all__'
+        
--- VueDjAgency/api2/views.py
+++ VueDjAgency/api2/views.py
@@ -1,13 +1,39 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from api2.serializers import UserSerializer, PostSerializer
-from blog.models import Post
+# from django.contrib.auth.models import User
+# from rest_framework import viewsets
+# from api2.serializers import UserSerializer, PostSerializer, CommentSerializer
+# from blog.models import Post, Comment
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# # ViewSets define the view behavior.
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
     
-class PostViewSet(viewsets.ModelViewSet):
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+    
+# class CommentViewSet(viewsets.ModelViewSet):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
+
+from blog.models import Post, Comment
+from api2.serializers import PostListSerializer, PostRetrieveSerializer, CommentSerializer
+
+class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostRetrieveSerializer
+
+class CommentCreateAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    
+class PostLikeAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    
