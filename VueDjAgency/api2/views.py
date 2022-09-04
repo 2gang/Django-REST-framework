4f93b368b6522d1fc9a144e7260e42198cf6e9ea
이경호 <rud0503@gmail.com>
2022-09-04 15:53:06 +0900

    0904

--- VueDjAgency/api2/views.py
+++ VueDjAgency/api2/views.py
@@ -20,58 +20,18 @@ from collections import OrderedDict
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from api.utils import obj_to_comment, obj_to_post, prev_next_post
 from blog.models import *
 from rest_framework.pagination import PageNumberPagination
 # from api2.serializers import PostListSerializer, PostRetrieveSerializer, CommentSerializer, PostLikeSerializer, CateTagSerializer
 from api2.serializers import PostListSerializer, CommentSerializer, CateTagSerializer, PostSerializerDetail
 
-# class PostListAPIView(ListAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostListSerializer
 
-# class PostRetrieveAPIView(RetrieveAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostRetrieveSerializer
+# class CommentCreateAPIView(CreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
 
-class CommentCreateAPIView(CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    
-# class PostLikeAPIView(UpdateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostLikeSerializer
-    
-#     #PATCH method
-#     def update(self, request, *args, **kwargs):
-#         partial = kwargs.pop('partial', False)
-#         instance = self.get_object()
-#         # data = instance.like + 1        
-#         data = {'like' : instance.like + 1 }    #좋아요 1 증가 코드
-#         serializer = self.get_serializer(instance, data=data, partial=partial)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-
-#         if getattr(instance, '_prefetched_objects_cache', None):
-#             # If 'prefetch_related' has been applied to a queryset, we need to
-#             # forcibly invalidate the prefetch cache on the instance.
-#             instance._prefetched_objects_cache = {}
-
-#         # return Response(serializer.data)
-#         return Response(data['like'])   #숫자만 보내고 싶을 때
-
-class PostLikeAPIView(GenericAPIView):
-    queryset = Post.objects.all()
-    # serializer_class = PostLikeSerializer
-    
-    #GET method
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.like += 1        
-        instance.save()
-
-        # return Response(serializer.data)
-        return Response(instance.like)   #숫자만 보내고 싶을 때
     
 class CateTagAPIView(APIView):
     def get(self, request, *args, **kwargs):
@@ -97,53 +57,12 @@ class PostPageNumberPagination(PageNumberPagination):
             ('pageCnt', self.page.paginator.num_pages),
             ('curPage', self.page.number),
         ]))
-
-
-class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
-    pagination_class = PostPageNumberPagination
-    
-    def get_serializer_context(self):
-        """
-        Extra context provided to the serializer class.
-        """
-        return {
-            'request': None,
-            'format': self.format_kwarg,
-            'view': self
-        }
-
-def get_prev_next(instance):
-    try:
-        prev = instance.get_previous_by_update_dt()
-    except instance.DoesNotExist:
-        prev = None
         
-    try:
-        next_ = instance.get_next_by_update_dt()
-    except instance.DoesNotExist:
-        next_ = None
-    
-    return prev, next_
-
-# serializer 사용      
-# class PostRetrieveAPIView(RetrieveAPIView):
+        
+# class PostListAPIView(ListAPIView):
 #     queryset = Post.objects.all()
-#     serializer_class = PostSerializerDetail
-    
-#     def retrieve(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         prevInstance, nextInstance = get_prev_next(instance)
-#         commentList = instance.comment_set.all()
-#         data = {
-#             'post' : instance,
-#             'prevPost' : prevInstance,
-#             'nextPost' : nextInstance,
-#             'commentList' : commentList,
-#         }
-#         serializer = self.get_serializer(instance = data )
-#         return Response(serializer.data)
+#     serializer_class = PostListSerializer
+#     pagination_class = PostPageNumberPagination
     
 #     def get_serializer_context(self):
 #         """
@@ -155,14 +74,64 @@ def get_prev_next(instance):
 #             'view': self
 #         }
         
-# serializer 사용 안하고 Dictionary를 만들어 사용      
-class PostRetrieveAPIView(RetrieveAPIView):
+        
+# # serializer 사용 안하고 Dictionary를 만들어 사용      
+# class PostRetrieveAPIView(RetrieveAPIView):
+#     def get_queryset(self):
+#         return Post.objects.all().select_related('category').prefetch_related('tags', 'comment_set')
+    
+#     def retrieve(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         commentList = instance.comment_set.all()
+        
+#         postDict = obj_to_post(instance)
+#         prevDict, nextDict = prev_next_post(instance)
+#         commentDict = [obj_to_comment(c) for c in commentList]
+        
+#         dataDict = {
+#             'post' : postDict,
+#             'prevPost' : prevDict,
+#             'nextPost' : nextDict,
+#             'commentList' : commentDict
+#         }
+        
+#         return Response(dataDict)
+    
+# class PostLikeAPIView(GenericAPIView):
+#     queryset = Post.objects.all()
+#     # serializer_class = PostLikeSerializer
+    
+#     #GET method
+#     def get(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         instance.like += 1        
+#         instance.save()
+
+#         # return Response(serializer.data)
+        
+        
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination
+    
+    #PostList
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': None,
+            'format': self.format_kwarg,
+            'view': self
+        }
+    
+    #PostRetrive
     def get_queryset(self):
         return Post.objects.all().select_related('category').prefetch_related('tags', 'comment_set')
     
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-        prevInstance, nextInstance = get_prev_next(instance)
         commentList = instance.comment_set.all()
         
         postDict = obj_to_post(instance)
@@ -177,3 +146,14 @@ class PostRetrieveAPIView(RetrieveAPIView):
         }
         
         return Response(dataDict)
+    
+    #PostLike
+    def like(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.like += 1        
+        instance.save()
+        return Response(instance.like)   #숫자만 보내고 싶을 때
+        
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
